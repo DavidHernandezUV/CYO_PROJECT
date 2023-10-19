7fab2d1a9ec682c73cc227a446775a1589ee4bf9
DavidHernandezUV <josedavid_0727@outlook.com>
2023-10-18 22:21:32 -0500

    high regime ended

--- PlantaGUI-Fuentes/main.py
+++ PlantaGUI-Fuentes/main.py
@@ -4,18 +4,21 @@ from minizinc import Instance, Model, Solver
 import matplotlib
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.gridspec import GridSpec
 
 matplotlib.use("TkAgg")
 def config_gui():
     
     #CONSTANTES
-    MIN_VALUE_PER_CLIENT = 0.5
     PROD_COST_N = 15
     PROD_COST_H = 25
     PROD_COST_T = 23
     CAP_N = 1000
     CAP_H = 300
     CAP_T = 500
+    MIN_VALUE_PER_CLIENT = 0.5
+    PRAH_VALUE_PER_CLIENT = 0.8
+    DCH_VALUE_PER_CLIENT = 2
     
 
     input_window = tk.Tk()
@@ -137,6 +140,20 @@ def config_gui():
     v_min = tk.Entry(input_widgets,width=10)
     v_min.insert(0,MIN_VALUE_PER_CLIENT)
     v_min.grid(row=9,column=1)
+    #*******************************************************************************************
+    #Porcentaje de capacidad del régimen alto en la hidroeléctrica
+    prah_label = tk.Label(input_widgets, text=f"Capacidad de H (%):")
+    prah_label.grid(row=9,column=2,sticky="w",pady=5,)
+    prah = tk.Entry(input_widgets,width=10)
+    prah.insert(0,PRAH_VALUE_PER_CLIENT)
+    prah.grid(row=9,column=3)
+    #*******************************************************************************************
+    #Cantidad de días consecutivos que se prohíbe a H permanecer en régimen alto
+    dch_label = tk.Label(input_widgets, text=f"Días consecutivos:")
+    dch_label.grid(row=9,column=4,sticky="w",pady=5,)
+    dch = tk.Entry(input_widgets,width=10)
+    dch.insert(0,DCH_VALUE_PER_CLIENT)
+    dch.grid(row=9,column=5)
     #Ejecución del solver
     def solve():
         daily_demand_table = table.get_sheet_data(return_copy=True)
@@ -148,18 +165,21 @@ def config_gui():
         n_cost_input_value = int(n_cost_input.get())
         t_cost_input_value = int(t_cost_input.get())
         v_min_value = float(v_min.get())
+        dch_value = int(dch.get())
+        prah_value = float(prah.get())
         costs = [n_cost_input_value,h_cost_input_value,t_cost_input_value]
         capacities = [n_cap_input_value,h_cap_input_value,t_cap_input_value]
-        apply_solver(costs,capacities,daily_demand_table,mw_prices_per_client,v_min_value)
+        apply_solver(costs,capacities,daily_demand_table,mw_prices_per_client,v_min_value,dch_value,prah_value)
+    
         
     input_widgets.grid(row=0, column=0, padx=5, pady=5)
     #Botón de resolver
     button = tk.Button(input_widgets, text="Solve", command=solve)
-    button.grid(row=9, column=2, padx=5, pady=5, columnspan=5, sticky="e")
+    button.grid(row=9, column=6, padx=5, pady=5, columnspan=5, sticky="e")
 
     input_window.mainloop()
-def apply_solver(costs,capacities,daily_demand_table,mw_prices_per_client,v_min_value):
-    model = Model("../PlantaEnergiaSimple.mzn")
+def apply_solver(costs,capacities,daily_demand_table,mw_prices_per_client,v_min_value,dch_value,prah_value):
+    model = Model("../PlantaEnergia.mzn")
     solver = Solver.lookup("coin-bc")
     instance = Instance(solver, model)
     d_bi = [row[1:] for row in daily_demand_table]
@@ -175,6 +195,8 @@ def apply_solver(costs,capacities,daily_demand_table,mw_prices_per_client,v_min_
     instance["d"] = d #Demanda diaria de cada cliente
     instance["Vmin"] = v_min_value #Venta mínima de energía a cada cliente si la producción no alcanza a cubrir la demanda
     instance["precio"] = precio  #Precio de MW para cada cliente
+    instance["PRAH"] = prah_value #Porcentaje de capacidad del régimen alto en la hidroeléctrica (H).
+    instance["DCH"] = dch_value #Cantidad de días consecutivos que se prohíbe a H permanecer en régimen alto.
     result = instance.solve()
     #print(result.solution.produccion)
     print(result)
@@ -187,9 +209,9 @@ def show_results(result):
 
     # Crear una figura con una cuadrícula de 2x2
     figure = Figure(figsize=(10, 8), dpi=100)
-
+    gs=GridSpec(3,4) # 2 rows, 3 columns
     #*********************************PRODUCTION*****************************************
-    subplot1 = figure.add_subplot(2, 1, 2)
+    subplot1 = figure.add_subplot(gs[0,2:])
     production = result.solution.produccion
     days = list(range(1,len(production[0])+1))
     colors = ['b', 'g', 'r']
@@ -205,10 +227,10 @@ def show_results(result):
     #*********************************Main function*****************************************
     main_function_label = tk.Label(results_window, text='Máxima ganancia',bg="white",font=("Arial", 16))
     main_function_label.pack()
-    f = tk.Label(results_window, text="$ "+str(result.solution.f),bg="white",font=("Arial", 16))
+    f = tk.Label(results_window, text="$ "+str(round(result.solution.f,2)),bg="white",font=("Arial", 16))
     f.pack()
     #*********************************daily total cost*****************************************
-    subplot3 = figure.add_subplot(2, 2, 1)
+    subplot3 = figure.add_subplot(gs[0,0])
     daily_cost = result.solution.costo_total_diario
     days = list(range(1, len(daily_cost) + 1))
     bars = subplot3.bar(days, daily_cost, align='center')
@@ -221,7 +243,7 @@ def show_results(result):
                         xytext=(0, 3), textcoords='offset points', ha='center', va='bottom', fontsize=7)
 
     #*********************************daily total income*****************************************
-    subplot4 = figure.add_subplot(2, 2, 2)
+    subplot4 = figure.add_subplot(gs[0,1])
     daily_income = result.solution.ingresos_diarios
     days = list(range(1, len(daily_income) + 1))
     bars = subplot4.bar(days, daily_income, align='center')
@@ -232,7 +254,56 @@ def show_results(result):
         height = bar.get_height()
         subplot4.annotate(f'{cost:.1f}', xy=(bar.get_x() + bar.get_width() / 2, height+4),
                         xytext=(0, 3), textcoords='offset points', ha='center', va='bottom', fontsize=7)
+        
+    
+    #*********************************EV*****************************************
+    subplot5 = figure.add_subplot(gs[1,:])
+    EV = result.solution.EV
+    EV = [[round(value, 2) for value in row] for row in EV]
+    empty_row = [None] * len(EV[0])
+    EV.insert(0, empty_row)
+    table = subplot5.table(cellText=EV, loc='center', cellLoc='center')
+    table.auto_set_font_size(False)
+    table.set_fontsize(10)
+    table.scale(1, 1.5)
 
+    rows_labels = [f'Cliente {i + 1}' for i in range(len(EV)-1)]
+    columns_labels = [f'Día {i + 1}' for i in range(len(EV[0]))]
+    for i, label in enumerate(rows_labels):
+        table.add_cell(i + 1, -1, 0.1, 0.1, text=label, loc='center', facecolor='lightgray')
+    for j, label in enumerate(columns_labels):
+        table.add_cell(0, j, 0.1, 0.1, text=label, loc='center', facecolor='lightgray')
+    subplot5.set_title('Energía vendida x cliente')
+    subplot5.axis('off')
+    # Mostrar la figura
+    figure.tight_layout()
+    #*********************************Daily Capacity H*****************************************
+    subplot6 = figure.add_subplot(gs[2,:2])
+    daily_cap_h = result.solution.cap_diaria_H
+    days = list(range(1,len(daily_cap_h)+1))
+    color = 'b'
+    label = 'Hidroeléctrica'
+    subplot6.plot(days, data, label=label, color=color)
+    for day, value in zip(days, daily_cap_h):
+        subplot6.text(day, value, f"{value:.1f}", ha='center', va='bottom', fontsize=8)
+    subplot6.legend()
+    subplot6.set_xlabel('Días')
+    subplot6.set_ylabel('MW')
+    subplot6.set_title('Capacidad por día')
+    #*********************************Consecutives days*****************************************
+    subplot7 = figure.add_subplot(gs[2,2:])
+    rah = result.solution.dias_consec_RAH
+    columns_labels = [f'Día {i + 1}' for i in range(len(rah))]
+    table = subplot7.table(cellText=[rah], loc='center', cellLoc='center',colLabels=columns_labels)
+    table.auto_set_font_size(False)
+    table.set_fontsize(10)
+    table.scale(1, 1.5)
+    
+
+    subplot7.set_title('Días consecutivos en régimen alto (Hidroeléctrica)')
+    subplot7.axis('off')
+    # Mostrar la figura
+    figure.tight_layout()
     # Agregar la figura a la ventana
     figure_canvas = FigureCanvasTkAgg(figure, results_window)
     figure_canvas.get_tk_widget().pack(padx=5, pady=5)
